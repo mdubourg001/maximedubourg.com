5e1c0be445807f23638c1fe9ab67d032b9077953
maxime.dubourg <maxime.dubourg@ekino.com>
2024-04-29 18:29:46 +0200

    feat: proper images sizes while loading to prevent CLS

--- creators/photographs.ts
+++ creators/photographs.ts
@@ -1,6 +1,7 @@
 import type { BuildPage, SsgoBag } from "https://deno.land/x/ssgo/mod.ts";
 import _ from "https://cdn.skypack.dev/lodash";
 import { walkSync } from "https://deno.land/std@0.118.0/fs/mod.ts";
+import sharp from "npm:sharp";
 
 export default async function (
   buildPage: BuildPage,
@@ -11,23 +12,31 @@ export default async function (
   watchDir(photosDir);
 
   const photosFiles: any[] = (Array.from(walkSync(photosDir)) as any[]).filter(
-    (e) => e.isFile
+    (e) => e.isFile && !e.name.endsWith(".thumb.webp")
   );
   const photos: any[] = [];
 
   for (const photo of photosFiles) {
-    if (photo.name.endsWith(".thumb.jpeg")) {
-      continue;
-    }
-
     const splittedNamed = photo.name.split(".");
+    const image = sharp(await Deno.readFile(photo.path));
 
-    photos.push({
-      path: `/photographs/${photo.name}`,
-      thumb: `/photographs/${splittedNamed[0]}.thumb.jpeg`,
-      name: photo.name,
-      index: splittedNamed.length === 3 ? Number(splittedNamed[1]) : Infinity,
-    });
+    image
+      .resize(916)
+      .toFile(`${photosDir}/${splittedNamed[0]}.thumb.webp`)
+      .then(() => console.log("Resized thumb for", photo.name));
+
+    image.metadata().then((metadata) =>
+      photos.push({
+        path: `/photographs/${photo.name}`,
+        thumb: `photographs/${splittedNamed[0]}.thumb.webp`,
+        name: photo.name,
+        index: splittedNamed.length === 3 ? Number(splittedNamed[1]) : Infinity,
+        thumbWidth: 916,
+        thumbHeight: Math.floor((metadata.height * 916) / metadata.width),
+        originalWidth: metadata.width,
+        originalHeight: metadata.height,
+      })
+    );
   }
 
   photos.sort((a, b) => a.index - b.index);
