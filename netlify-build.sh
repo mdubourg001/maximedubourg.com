a7028e409a29bb061008273da483f5a43f41e3ab
maxime.dubourg <maxime.dubourg@ekino.com>
2025-01-24 15:50:25 +0100

    chore: ditching velociraptor for a package.json

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,20 +1,18 @@
-echo "Installing nvm and node@14.3.0"
+echo "Installing nvm, node@20.0 and installing dependencies..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 20
+npm install
 
 echo "Installing deno..."
 curl -fsSL "https://deno.land/install.sh" | sh -s -- v1.46.3
 export PATH="/opt/buildhome/.deno/bin:$PATH"
 
-echo "Installing velociraptor..."
-deno install -qA -n vr https://deno.land/x/velociraptor@1.5.0/cli.ts
 
 echo "Installing ssgo..."
 deno install --reload -f --unstable -A https://deno.land/x/ssgo/ssgo.ts
 
 echo "Building website..."
-NODE_ENV=production npx tailwindcss@latest -c ./tailwind.config.js -o ./static/tailwind.css
-ssgo --sitemap=https://maximedubourg.com
+npm run build
